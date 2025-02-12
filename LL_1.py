bd02be5e13519cd3e90c28b203743a91bce43a74
ArkomsT <artemijs.terlanovs@edu.rtu.lv>
2025-02-12 14:02:19 +0200

    Update LL_1.py

--- LL_1.py
+++ LL_1.py
@@ -21,13 +21,17 @@ class LinkedList:
             self.tail = new_node
         return True
         
+def find_middle_node(self):  
+        
+        slow = self.head
+        fast = self.head
 
-    # WRITE FIND_MIDDLE_NODE METHOD HERE #
-    #                                    #
-    #                                    #
-    #                                    #
-    #                                    #
-    ######################################
+        while fast is not None and fast.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow 
+    
 
 
 
